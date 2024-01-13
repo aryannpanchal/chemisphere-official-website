631944938671e97d50556affffc24e8c97edadc7
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-13 12:37:57 +0530

    Update Footer.jsx
    fixed footer link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
 <footer className=" rounded-lg shadow  m-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <a href="https://chemisphere.in/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <Image src="chemisphere-black.svg" className="h-8" alt="Chemisphere Logo" height={100} width={100} />
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-900 sm:mb-0 dark:text-gray-900">
