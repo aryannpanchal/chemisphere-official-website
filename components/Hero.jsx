2142a7eacb62d49e05ac45e363ae4ef6c0ffcdcb
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-13 12:36:35 +0530

    Update Hero.jsx
    removed customized classes span

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
         </h1>
         <p className="text-gray-900 max-w-xl leading-relaxed sm:mx-auto lg:ml-0 md:text-left">
         Hi. I am Chandan Biswas. I am a professor of Chemistry, and CEO, Chemisphere and I have mentored over 15000 students in the last decade for JEE Main and Advanced, NEET, MHCET and Class 12th boards and I know inside out of all these exams. I have the honor to be the mentor for over 1000 IITians, and medical professionals.
-<br /><span className="font-bold text-chemisphere">We customize classes according to your needs! </span></p>
+<br /></p>
          <div className="flex pt-0 mt-0 items-center justify-center  md:space-y-0 md:mt-0 md:pb-0 md:justify-start">
                       <Link target="__blank" href=" https://wa.me/+918850436230?text=I'm%20interested%20in%20inquiring%20more%20about%20Chemisphere!"  className="rounded-full transition ease-in-out duration-300">
                       <Image src="/whatsapp chat.svg" width={194} height={194} alt="chat on whatsapp button"  />
