4accccdaea8faf7f19675ee7277625bde13b03fe
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-15 21:18:13 +0530

    Update Mockup.jsx

--- components/Mockup.jsx
+++ components/Mockup.jsx
@@ -5,18 +5,9 @@ const mockup = () => {
   return (
     <> 
     <section className="text-gray-600 body-font md:hidden block">
-    <div className="container mx-auto flex px-5 pt-14 md:flex-row flex-col items-center">
-    <div className="lg:max-w-lg lg:w-full md:w-1/4 w-5/7 mb-10 md:mb-0">
-      
-    <div class="relative mx-auto border-slate-900 dark:border-slate-900 bg-chemisphere dark:bg-chemisphere border-[14px] rounded-[2.5rem] h-[600px] w-[300px]">
-    <div class="h-[32px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
-    <div class="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
-    <div class="h-[46px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
-    <div class="h-[64px] w-[3px] bg-gray-800 dark:bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
-    <div class="rounded-[2rem] overflow-hidden bg-white dark:bg-gray-800">
-        <img src="/mockup.png" class="" alt=""></img>
+    <div>
+        <Image src='/c-logo.svg' alt='mobile-image' width={500} height={500} className='flex items-center justify-center' />
     </div>
-</div>
 <div className="w-full p-4 mt-6 text-center bg-chemisphere border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-chemisphere dark:border-chemisphere">
       <h5 className="mb-2 text-3xl font-bold text-white dark:text-white">Entire student dashboard, now on your mobile.</h5>
      
