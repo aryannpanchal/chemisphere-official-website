4996e6301f96e52d83f2d7a74a5dcf8554f63003
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-15 22:48:28 +0530

    Update FAQ.jsx

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -7,19 +7,24 @@ const FAQ = () => {
      <div className='text-center text-2xl font-bold mt-20 mb-5'>
       <h1>FAQ</h1>
     </div>
-    <div className="p-4 bg-chemisphere rounded-lg   text-white">
+    <div className="p-4 bg-chemisphere rounded-lg text-white">
      
     <Accordion
         title="Why choose Chemisphere?"
         answer="Choose Chemisphere for a captivating chemistry learning experience! Learn from professionals with a decade of mentoring success. Enjoy engaging live classes, meticulous class notes, and track progress with regular assessments and examinations. Elevate your marks and embrace the joy of learning chemistry!"
+        className='font-semibold'
       />
       <Accordion
         title="How many classes happen in a week?"
-        answer=""
+        answer="a"
+        className='font-semibold'
+
       />
       <Accordion
         title="question 3" 
-        answer="" />
+        answer="a" 
+        className='font-semibold'
+/>
     </div>
     </div>
   );
