ff8e6725bcee8cc12efd35893a225ed2abdcb619
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-16 21:19:58 +0530

    Update page.js

--- app/about-us/page.js
+++ app/about-us/page.js
@@ -2,15 +2,19 @@ import React from 'react'
 import Navbar from './../../components/Navbar';
 import AboutUsPageBreadcrumb from './../../components/AboutUsPageBreadcrumb';
 import AboutUsHome from './../../components/AboutUsHome'
+import Footer from './../../components/Footer'
 
-const blogs = () => {
+
+
+const aboutUs = () => {
   return (
     <div>
       <Navbar />
       <AboutUsPageBreadcrumb />
       <AboutUsHome />
+      <Footer />
     </div>
   )
 }
 
-export default blogs
+export default aboutUs
