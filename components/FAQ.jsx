f233b1b9d8e17bd08e482e6237d8a69601038d6e
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-20 10:36:20 +0530

    Update FAQ.jsx

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -19,12 +19,12 @@ const FAQ = () => {
           className='lg:text-left'
         />
         <Accordion
-          title={<h1 className='font-bold'>Will there be tests? How will they be taken?</h1>}
+          title={<h1 className='font-bold'>How will tests be conducted?</h1>}
           answer="Our coaching program leverages technology to enhance the learning experience. The Android/iOS app facilitates a convenient platform for conducting tests and enables parents to track their child's performance. This integration of technology ensures a modern and efficient learning environment. All the reports of the tests are also shared with the parent so that their performance is tracked."
           className='lg:text-left'
         />
         <Accordion
-          title={<h1 className='font-bold'>How do we know my child is in safe hands?</h1>}
+          title={<h1 className='font-bold'>Will our child be in safe hands?</h1>}
           answer="As an IIT Madras graduate with 12 years of teaching experience in various coaching institutes across India, I bring a unique perspective to the table. Unlike some coaching institutes that prioritize completing the syllabus and little focus beyond that, my focus is on understanding individual student needs and providing quality education. My experience allows me to guide students not just through the curriculum sooner but also to help them develop a deeper understanding of the subjects."
           className='lg:text-left'
         />
