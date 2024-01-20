fb11f3ecf16a2943fa586ae71533e51c035fb562
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-20 18:25:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,20 @@
-import type { Metadata as NextMetadata } from 'next'
+import type { Metadata } from 'next'
 import './globals.css'
 import { Inter } from "@next/font/google"
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+ import { SpeedInsights } from "@vercel/speed-insights/next"
 
-interface Metadata extends NextMetadata {
-  image?: string;
-}
 
 const inter = Inter({
   subsets: ['latin'],
-  weight: ['400', '700'],
+  weight: ['400','700'],
+  
 })
 
 export const metadata: Metadata = {
   title: 'Chemisphere. Chemistry made easy!',
   description: 'Premium Chemistry Lectures by Chandan Biswas',
-  // Add OG image URL based on your public folder structure
-  image: '/meta.png',
+ 
 }
 
 export default function RootLayout({
@@ -27,21 +24,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href='/favicon.ico' sizes="any" />
-        {/* Add Open Graph meta tags */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-      </head>
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-        <SpeedInsights />
-      </body>
+     <head>
+     <link rel="icon" href='/favicon.ico' sizes="any" />
+     </head>
+      <body className={inter.className}>{children}<Analytics /><SpeedInsights/></body>
     </html>
   )
 }
+
 
 
 
