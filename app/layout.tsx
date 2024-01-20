5c739c921dcb90b6d94bf0c149c7ed7f31842137
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-20 19:53:29 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,26 @@ export default function RootLayout({
   return (
     <html lang="en">
      <head>
+      <!-- HTML Meta Tags -->
+<title>Chemisphere. Chemistry made easy!</title>
+<meta name="description" content="Premium Chemistry Lectures by Chandan Biswas">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://www.chemisphere.in">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Chemisphere. Chemistry made easy!">
+<meta property="og:description" content="Premium Chemistry Lectures by Chandan Biswas">
+<meta property="og:image" content="/meta.png">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="chemisphere.in">
+<meta property="twitter:url" content="https://www.chemisphere.in">
+<meta name="twitter:title" content="Chemisphere. Chemistry made easy!">
+<meta name="twitter:description" content="Premium Chemistry Lectures by Chandan Biswas">
+<meta name="twitter:image" content="/meta.png">
+
+<!-- Meta Tags Generated via https://www.opengraph.xyz -->
      <link rel="icon" href='/favicon.ico' sizes="any" />
      </head>
       <body className={inter.className}>{children}<Analytics /><SpeedInsights/></body>
