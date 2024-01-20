530a5c9b7a9c129c2416d3ea5af13b9d013283eb
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-20 17:30:32 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
 import type { Metadata } from 'next'
-import './globals.css'
-import { Inter } from "@next/font/google"
+import Head from 'next/head';
+import { Inter } from "@next/font/google";
 import { Analytics } from '@vercel/analytics/react';
- import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ['latin'],
   weight: ['400','700'],
-})
+});
 
 export const metadata: Metadata = {
   title: 'Chemisphere. Chemistry made easy!',
   description: 'Premium Chemistry Lectures by Chandan Biswas',
-  
-}
+  image: '/apple-touch-icon.png', // Replace with the actual path to your image
+};
 
 export default function RootLayout({
   children,
@@ -23,13 +22,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-     <head>
-        <meta property="og:image" content="/chandan.svg" />
-        <meta property="og:url" content="https://www.chemisphere.in" />
+      <Head>
+        <link rel="icon" href='/favicon.ico' sizes="any" />
+
+        {/* Open Graph tags for social media sharing */}
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:url" content="https://www.chemisphere.in" /> {/* Replace with the actual URL of your page */}
         <meta property="og:type" content="website" />
-     <link rel="icon" href='/favicon.ico' sizes="any" />
-     </head>
-      <body className={inter.className}>{children}<Analytics /><SpeedInsights/></body>
+
+        <style>{inter.styles}</style>
+      </Head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
-  )
+  );
 }
