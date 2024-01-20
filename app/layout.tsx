1eb95594d5bfa3eb67febd31c88bae7625f576d5
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-20 17:55:21 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,33 @@
-import type { Metadata } from 'next';
-import Head from 'next/head';
-import { Inter } from "@next/font/google";
+import type { Metadata } from 'next'
+import './globals.css'
+import { Inter } from "@next/font/google"
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+ import { SpeedInsights } from "@vercel/speed-insights/next"
 
-// Extend the Metadata type to include the 'image' property
-interface ExtendedMetadata extends Metadata {
-  image?: string;
-}
 
 const inter = Inter({
   subsets: ['latin'],
   weight: ['400','700'],
-});
+})
 
-export const metadata: ExtendedMetadata = {
+export const metadata: Metadata = {
   title: 'Chemisphere. Chemistry made easy!',
   description: 'Premium Chemistry Lectures by Chandan Biswas',
-  image: '/meta.png', // Replace with the actual path to your image
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href='/favicon.ico' sizes="any" />
-
-        {/* Open Graph tags for social media sharing */}
-        <meta property="og:title" content={metadata.title ?? ''} />
-        <meta property="og:description" content={metadata.description?.toString() ?? ''} />
-        {metadata.image && <meta property="og:image" content={`https://www.chemisphere.in${metadata.image}`} />}
-        <meta property="og:url" content="https://www.chemisphere.in" />
-        <meta property="og:type" content="website" />
-
-        <style>{inter.styles}</style>
-      </Head>
-      <body className={inter.className}>
-        {children}
-        <Analytics />
-        <SpeedInsights />
-      </body>
+     <head>
+     <link rel="icon" href='/favicon.ico' sizes="any" />
+     </head>
+      <body className={inter.className}>{children}<Analytics /><SpeedInsights/></body>
     </html>
-  );
+  )
 }
+
 
