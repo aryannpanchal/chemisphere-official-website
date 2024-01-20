8bd7e4f336ca83dfddcd2b9a15a3a495aa450c68
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-20 09:40:39 +0530

    Update MarqueeSection.jsx

--- components/MarqueeSection.jsx
+++ components/MarqueeSection.jsx
@@ -8,7 +8,7 @@ const MarqueeSection = () => {
       <Marquee className='text-white bg-chemisphere'>
         <span>
           NEET 2024 Chemistry crash course admissions are NOW OPEN!{' '}
-          <a className='underline' href="https://wa.me/+918850436230?text=Im%20interested%20in%20inquiring%20more%20about%20the%20NEET%202024%20Crash%20Course%20offered%20by%20Chemisphere!">
+          <a className='underline' href="https://wa.me/+918850436230?text=I'm%20interested%20in%20inquiring%20more%20about%20the%20NEET%202024%20Crash%20Course%20offered%20by%20Chemisphere!">
             CLICK HERE
           </a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {/* Add a non-breaking space here */}
         </span>
