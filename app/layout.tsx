6cfb4c5d8a6a367ada3751bca3a174cdc9fae043
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-01-24 15:35:28 +0530

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Inter } from "@next/font/google"
 import { Analytics } from '@vercel/analytics/react';
- import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Plus_Jakarta_Sans } from "@next/font/google"
 
 
-const inter = Inter({
+
+const inter = Plus_Jakarta_Sans({
   subsets: ['latin'],
   weight: ['400','700'],
   
