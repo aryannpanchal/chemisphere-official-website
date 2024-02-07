1ca462b6fa656f179a7bf68dd0f12d67495bc0a7
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-02-07 18:47:15 +0530

    .
    .

--- components/NavbarSection.jsx
+++ components/NavbarSection.jsx
@@ -1,9 +1,10 @@
-'use client'
 import {
   Navbar,
   NavbarBrand,
   Avatar,
-  Dropdown
+  Dropdown,
+  DropdownItem,
+  DropdownDivider
 } from 'flowbite-react';
 
 import Link from'next/link'
@@ -23,7 +24,7 @@ function NavbarSection() {
        
     
     <div class=" w-full" id="navbar-dropdown">
-      <ul class="flex flex-row font-medium  border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
+      <ul class="flex flex-row font-medium md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 ">
       {/* social links  */}
       <Link href='https://www.youtube.com/@Chemisphere' target="__blank">
         <Avatar alt="youtube-logo" img="/yt.svg" className='w-6 h-6 mr-5'/>
@@ -34,29 +35,24 @@ function NavbarSection() {
       <Link href='https://www.instagram.com/chemisphere.in/' target="__blank">
         <Avatar alt="instagram-logo" img="/instagram.svg" className='w-6 h-6 mr-5'/>
       </Link>
-            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex-row items-center justify-between w-full py-2 pl-3 text-gray-900 rounded md:p-0"><svg className=
-            "w-3 h-fit lg:w-5 lg:h-fit" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-    <path stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-  </svg></button>
-            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 ">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                  <li>
-                    <a href="about-us" class="block px-4 py-2 hover:bg-gray-100 ">About us</a>
-                  </li>
-                  <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100  ">JEE Main portal</a>
-                  </li>
-                  <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100  ">JEE Advanced portal</a>
-                  </li>
-                  <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100  ">NEET portal</a>
-                  </li>
-                </ul>
-                <div class="py-1">
-                  <a href="privacy-policy" class="block px-4 py-2 text-sm  hover:bg-gray-100">Privacy policy</a>
-                </div>
-            </div>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="/bar.svg" className='md:w-fit md:h-fit w-5 h-5'/>
+          }
+        >
+         
+          <DropdownItem href='about-us' className='hover:bg-chemisphere'>About Us</DropdownItem>
+          <DropdownItem href='' className='hover:bg-chemisphere'>JEE Main Portal</DropdownItem>
+          <DropdownItem href=''className='hover:bg-chemisphere'>JEE Advanced Portal</DropdownItem>
+          <DropdownItem href=''className='hover:bg-chemisphere'>NEET Portal</DropdownItem>
+
+          <DropdownDivider />
+          <DropdownItem href='privacy-policy'className='hover:bg-chemisphere'>Privacy Policy</DropdownItem>
+        </Dropdown>
+      </div>
      
         
       
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-     
+      
+        darkMode: 'class',
+       
         root: {
           "base": "fixed top-0 right-0 left-0 z-50 h-modal h-screen overflow-y-auto overflow-x-hidden md:inset-0 md:h-full",
           "show": {
