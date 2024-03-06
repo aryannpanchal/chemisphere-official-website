5c5d813fcff58e3164142329b3b92fbdb4a964bd
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-03-06 21:09:09 +0530

    Update JeePortal.jsx

--- components/JeePortal.jsx
+++ components/JeePortal.jsx
@@ -6,13 +6,13 @@ const JeePortal = () => {
     <>
     
 
-    <div id="informational-banner" tabindex="-1" class=" start-0 z-50 flex flex-col justify-between w-full p-4 border-b border-gray-200 md:flex-row ">
-    <div class="m-5 md:mb-0 md:me-4">
+    <div id="informational-banner" tabindex="-1" class=" start-0 z-50 flex justify-center w-full p-4 ">
+    <div class="mt-5 md:mb-0 md:me-4 flex-row">
         <h2 class="mb-1 text-base font-semibold text-gray-900 dark:text-white">Download the JEE MAIN INFORMATION BULLETIN 2024</h2>
         <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">The JEE MAIN INFORMATION BULLETIN provides an A to Z brief on the examination.</p>
       <div class="flex items-center flex-shrink-0 pt-3">
        
-        <a href="https://jeemain.nta.ac.in/images/information-bulletin-for-jee-main-2024.pdf" class="inline-flex items-center justify-center px-3 py-2 me-2 text-xs font-medium text-white bg-chemisphere rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-chemisphere dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Download</a>
+        <a href="https://jeemain.nta.ac.in/images/information-bulletin-for-jee-main-2024.pdf" class="inline-flex items-center justify-center px-3 py-2 me-2 text-xs font-medium text-white bg-chemisphere rounded-lg hover:bg-black focus:ring-4 focus:ring-blue-300 dark:bg-chemisphere dark:hover:bg-black focus:outline-none dark:focus:ring-blue-800">Download</a>
         <button data-dismiss-target="#informational-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
