9984e8ff9db4902cdf272867103489b8dbe0c191
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-03-06 22:18:40 +0530

    Create Indicator.jsx

---
+++ components/Indicator.jsx
@@ -0,0 +1,18 @@
+'usse client';
+
+import { motion, useScroll, useSpring } from "framer-motion";
+
+const Indicator = () => {
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+  return (
+    <motion.div className="bg-chemisphere h-1 fixed top-0 left-0 right-0 z-50" style={{ scaleX }} />
+
+  )
+}
+
+export default Indicator
