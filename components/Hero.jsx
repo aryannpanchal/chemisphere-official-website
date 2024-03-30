2f06d099807d3ea20b214035ad852c8c15bdb78f
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-03-30 14:45:04 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -22,9 +22,10 @@ const Hero = () => {
     <section className="lg:pt-6 mt-4 lg:mt-0 lg:pb-24 gap-14 lg:gap-0 pb-10 items-center flex-col flex md:px-8 md:flex-row text-left m-2 md:text-center lg:bg-none bg-cover bg-center lg:mr-14 ">
         
     <div className="space-x-xl flex-1   ">
-       <h1 className="text-slate-800 font-extrabold lg:text-6xl text-4xl mb-6 tracking-tight ">
-            Premium chemistry coaching <br/>for JEE (Main & Advanced), <br />NEET & Boards.
+       <h1 className="text-slate-800 font-extrabold lg:text-6xl text-4xl mb-3 tracking-tight ">
+            Chemisphere by Chandan Biswas
         </h1>
+        <h2 className='text-slate-800 font-normal lg:text-5xl text-3xl mb-6 tracking-tight '> Premium chemistry coaching for JEE (Main & Advanced),<br /> NEET & Boards.</h2>
         <p className='lg:text-6xl text-4xl font-extrabold text-chemisphere tracking-tighter'>ADMISSIONS OPEN</p>
         {/* <p className="text-gray-900 max-w-xl leading-relaxed sm:mx-auto lg:ml-0 md:text-left">
         Hi. I am Chandan Biswas. I am a professor of Chemistry, and CEO, Chemisphere and I have mentored over 15000 students in the last decade for JEE Main and Advanced, NEET, MHCET and Class 12th boards and I know inside out of all these exams. I have the honor to be the mentor for over 1000 IITians, and medical professionals.
