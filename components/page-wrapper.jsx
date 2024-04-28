94f465160afcd080c96f424ab8206c49aacc7c9a
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-04-28 09:19:01 +0530

    Update page-wrapper.jsx

--- components/page-wrapper.jsx
+++ components/page-wrapper.jsx
@@ -2,14 +2,15 @@ import { motion, AnimatePresence } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
 export const PageWrapper = ({ children }) => {
+  const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.1 });
+
   return (
     <AnimatePresence>
       <motion.div
-        initial={{ opacity: 0, y: 5 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -5 }}
-        transition={{ delay: 0.25, ease: "easeIn" }}
-        style={{ position: "relative", overflow: "hidden" }}
+      ref={ref}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: inView ? 1 : 0 }}
+      transition={{ duration: 0.5 }}
       >
         {children}
       </motion.div>
