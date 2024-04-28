89462adf1605897465b7882a4cbed769bfad95fd
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-04-28 10:21:24 +0530

    .

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,73 +1,69 @@
-import { motion, AnimatePresence } from 'framer-motion';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
+"use client";
+import React, { useState, useEffect, useRef } from "react"
+import 'swiper/css/pagination';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { Label, TextInput } from "flowbite-react";
+import Link from 'next/link'
 
 const Hero = () => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
+   
     <div>
       <section>
-        <div className="grid px-10 mx-auto gap-8 xl:gap-0 laptop:py-8 lg:grid-cols-12 mb-24 mobile:mt-12 ">
-          <AnimatePresence>
-            {isMounted && (
-              <>
-                <motion.div
-                  initial={{ y: 50, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  transition={{ type: 'spring', stiffness: 100, damping: 20, ease: 'easeIn' }}
-                  className="mr-auto place-self-center lg:col-span-7"
-                >
-                  <h1 className="text-slate-900 font-extrabold laptop:text-6xl mobile:text-5xl mobile:text-center mb-3 tracking-tight laptop:text-left">
-                    Chemisphere by Chandan Biswas.
-                  </h1>
-                  <p className="mobile:text-center text-slate-800 laptop:text-5xl mobile:text-xl my-6 tracking-tight laptop:text-left font-extrabold">Premium chemistry coaching for JEE (Main & Advanced), NEET & Boards.</p>
-                  <p className='mobile:text-center laptop:text-6xl mobile:text-3xl font-extrabold text-chemisphere tracking-tighter laptop:text-left'>ADMISSIONS OPEN</p>
-                </motion.div>
-                <motion.div
-                  initial={{ y: 50, opacity: 0 }}
-                  animate={{ y: 0, opacity: 1 }}
-                  transition={{ type: 'spring', stiffness: 100, damping: 20, ease: 'easeIn' }}
-                  className="lg:mt-0 lg:col-span-5 flex justify-center"
-                >
-                  <div className='NeoButton shadow-3xl p-10 rounded-xl bg-white'>
-                    <div className="flex  flex-col gap-4">
-                      <p className='font-semibold text-center mobile:text-2xl laptop:text-3xl '>Book a free demo class NOW!</p>
-                      <div className='laptop:mx-20'>
-                        <img src='/chandan.svg' />
-                      </div>
-                      <div className='flex laptop:flex-row mobile:flex-col justify-center item-center laptop:gap-10 mobile:gap-5 mobile:pt-3 mobile:text-center laptop:pt-5'>
-                        <Link href="/sign-up">
-                          <button className=' NeoButton2 text-center hover:border-chemisphere  rounded-lg hover:bg-chemisphere hover:text-white transition p-2'>
-                            Book via form
-                          </button>
-                        </Link>
-                        <div>
-                          <div>
-                            <Link target='__blank' href="https://wa.me/+918850436230?text=I'm%20interested%20in%20inquiring%20about%20classes%20of%20Chemisphere!">
-                              <button className=' NeoButton2 text-center hover:border-chemisphere  rounded-lg hover:bg-chemisphere hover:text-white transition p-2'>
-                                Book via WhatsApp
-                              </button>
-                            </Link>
-                          </div>
-                          <p className='text-center py-2 text-gray-600'>⚡Faster way</p>
-                        </div>
-                      </div>
-                      <p className='font-base pt-2 text-gray-500 text-center text-sm '>On booking, you will agree to the terms and conditions of chemisphere.</p>
-                    </div>
-                  </div>
-                </motion.div>
-              </>
-            )}
-          </AnimatePresence>
+    <div class="grid px-10 mx-auto gap-8 xl:gap-0 laptop:py-8 lg:grid-cols-12 mb-24 mobile:mt-12 ">
+        <div class="mr-auto place-self-center lg:col-span-7 ">
+        <h1 className="text-slate-900 font-extrabold laptop:text-6xl mobile:text-5xl mobile:text-center mb-3 tracking-tight laptop:text-left">
+            Chemisphere by Chandan Biswas.
+        </h1>
+            <p class="mobile:text-center text-slate-800 laptop:text-5xl mobile:text-xl my-6 tracking-tight laptop:text-left font-extrabold">Premium chemistry coaching for JEE (Main & Advanced),<br /> NEET & Boards.</p>
+            <p className='mobile:text-center laptop:text-6xl mobile:text-3xl font-extrabold text-chemisphere tracking-tighter laptop:text-left'>ADMISSIONS OPEN</p>
         </div>
-      </section>
+        <div className=" lg:mt-0 lg:col-span-5 flex justify-center ">
+        <div className='NeoButton shadow-3xl p-10 rounded-xl bg-white'> 
+
+<div className="flex  flex-col gap-4 "
+>
+  <p className='font-semibold text-center mobile:text-2xl laptop:text-3xl '>Book a free demo class NOW!</p>
+  <div className='laptop:mx-20'>
+    <img src='/chandan.svg' />
+  </div>
+         <div className='flex laptop:flex-row mobile:flex-col justify-center item-center laptop:gap-10 mobile:gap-5 mobile:pt-3 mobile:text-center laptop:pt-5'>
+         <Link  href="/sign-up">
+          <button  className=' NeoButton2 text-center hover:border-chemisphere  rounded-lg hover:bg-chemisphere hover:text-white transition p-2'>
+          Book via form
+          </button>
+         </Link>
+         {/* <p className='text-center laptop:pt-2'>or</p> */}
+        <div>
+         <div>
+         <Link target='__blank' href="https://wa.me/+918850436230?text=I'm%20interested%20in%20inquiring%20about%20classes%20of%20Chemisphere!">
+          <button className=' NeoButton2 text-center hover:border-chemisphere  rounded-lg hover:bg-chemisphere hover:text-white transition p-2'>
+          Book via WhatsApp 
+          </button>
+         </Link>
+          </div>
+          <p className='text-center py-2 text-gray-600'>⚡Faster way</p>
+        </div> 
+        </div>         
+        <p className='font-base pt-2 text-gray-500 text-center text-sm '>On booking, you will agree to the terms and conditions of chemisphere.</p>
+ 
+       
+</div>
+
+
+   </div>        
+   </div>                
     </div>
+</section>
+       <div>
+        
+        
+    
+</div>
+</div>
+
   )
 }
 
-export default Hero;
+export default Hero
--- components/Mockup.jsx
+++ components/Mockup.jsx
@@ -1,91 +1,109 @@
+import { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
 
-const mockup = () => {
+const Mockup = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5, // When 50% of the element is visible
+  });
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <> 
-<section className="text-gray-600 body-font mb-24 ">
-    
-<div className=" p-4 py-16 mt-6 text-center rounded-lg sm:p-8  sm:flex-col md:flex gap-5 justify-center align-center items-center">
-      <div className='flex justify-center'>
-        <Image src='/mob2.png' alt='mobile-image' width={700} height={700} className='flex items-center justify-center' />
-    </div>
-    <div>
+    <>
+      <section ref={ref} className={`fade-in-section ${inView ? 'is-visible' : ''}`}>
+        <div className="text-gray-600 body-font mb-24">
+          <div className="p-4 py-16 mt-6 text-center rounded-lg sm:p-8  sm:flex-col md:flex gap-5 justify-center align-center items-center">
+            <div className='flex justify-center'>
+              <Image src='/mob2.png' alt='mobile-image' width={700} height={700} className='flex items-center justify-center' />
+            </div>
+            <div>
+              <h2 className="mb-2 text-2xl font-extrabold text-black mobile:pt-5">Entire student dashboard, now on your mobile.</h2>
 
- <h2 className="mb-2 text-2xl font-extrabold text-black mobile:pt-5">Entire student dashboard, now on your mobile.</h2>
-     
-     <p className="mb-5 laptop:text-base text-black mobile:pt-5 mx-6 mobile:text-l font-thin max-w-md">Our app is the go-to solution for students across the country with a simple user interface, design and exciting features.</p>
-      <div>
-         <ul role="list" class="space-y-4 text-black mobile:pt-5 pb-6 px-2 md:hidden block text-base">
-               <li class="flex space-x-2 rtl:space-x-reverse items-center">
-                   <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                   </svg>
-                   <span class="leading-tight text-base">Interactive Live Classes</span>
-               </li>
-               <li class="flex space-x-2 rtl:space-x-reverse items-center">
-                   <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                   </svg>
-                   <span class="leading-tight text-base">Course Material</span>
-               </li>
-               <li class="flex space-x-2 rtl:space-x-reverse items-center">
-                   <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                   </svg>
-                   <span class="leading-tight text-base">Tests and performance reports</span>
-               </li>
-               <li class="flex space-x-2 rtl:space-x-reverse items-center">
-                   <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                   </svg>
-                   <span class="leading-tight text-md">Doubt clearance portal open 24x7</span>
-               </li>
-               <li class="flex space-x-2 rtl:space-x-reverse items-center">
-                   <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                       <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
-                   </svg>
-                   <span class="leading-tight text-md">User friendly Interface</span>
-               </li>
-               <li class="flex rtl:space-x-reverse items-center">
-                   <span class=" leading-tight text-md text-gray-500">and much more.</span>
-               </li>
+              <p className="mb-5 laptop:text-base text-black mobile:pt-5 mx-6 mobile:text-l font-thin max-w-md">Our app is the go-to solution for students across the country with a simple user interface, design and exciting features.</p>
+              <div>
+                <ul role="list" className="space-y-4 text-black mobile:pt-5 pb-6 px-2 md:hidden block text-base">
+                  <li className="flex space-x-2 rtl:space-x-reverse items-center">
+                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="leading-tight text-base">Interactive Live Classes</span>
+                  </li>
+                  <li className="flex space-x-2 rtl:space-x-reverse items-center">
+                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="leading-tight text-base">Course Material</span>
+                  </li>
+                  <li className="flex space-x-2 rtl:space-x-reverse items-center">
+                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="leading-tight text-base">Tests and performance reports</span>
+                  </li>
+                  <li className="flex space-x-2 rtl:space-x-reverse items-center">
+                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="leading-tight text-md">Doubt clearance portal open 24x7</span>
+                  </li>
+                  <li className="flex space-x-2 rtl:space-x-reverse items-center">
+                    <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-400 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                    </svg>
+                    <span className="leading-tight text-md">User friendly Interface</span>
+                  </li>
+                  <li className="flex rtl:space-x-reverse items-center">
+                    <span className=" leading-tight text-md text-gray-500">and much more.</span>
+                  </li>
 
-               </ul>
-      </div>
-     
-     <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
-       <Link href="https://apps.apple.com/in/app/classplus/id1324522260" target='__blank'>
-         <div className=" w-full sm:w-auto bg-slate-900 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-slate-900 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-           <svg className="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg>
-           <div className="text-left rtl:text-right">
-             <div className="mb-1 text-xs">Download on the</div>
-             <div className="-mt-1 font-sans text-sm font-semibold">Mac App Store</div>
-           </div></div>
-         </Link>
-         <div>
-       <Link href="https://play.google.com/store/apps/details?id=co.lily.nydae" target='__blank'>
-         <div className="w-full sm:w-auto bg-slate-900 hover:bg-slate-900 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-slate-900 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-           <svg className="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google-play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"></path></svg>
-           <div className="text-left rtl:text-right">
-             <div className="mb-1 text-xs">Get in on</div>
-             <div className="-mt-1 font-sans text-sm font-semibold">Google Play</div>
-           </div>
-         </div>
-       </Link>
-       </div>
-       
-     </div> 
-    </div>
-    </div>
-   
-      
-   
-  </section>
- </>
-   
+                </ul>
+              </div>
+
+              <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
+                <Link href="https://apps.apple.com/in/app/classplus/id1324522260" target='__blank'>
+                  <div className=" w-full sm:w-auto bg-slate-900 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-slate-900 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                    <svg className="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="apple" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"></path></svg>
+                    <div className="text-left rtl:text-right">
+                      <div className="mb-1 text-xs">Download on the</div>
+                      <div className="-mt-1 font-sans text-sm font-semibold">Mac App Store</div>
+                    </div></div>
+                </Link>
+                <div>
+                  <Link href="https://play.google.com/store/apps/details?id=co.lily.nydae" target='__blank'>
+                    <div className="w-full sm:w-auto bg-slate-900 hover:bg-slate-900 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-slate-900 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                      <svg className="me-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google-play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"></path></svg>
+                      <div className="text-left rtl:text-right">
+                        <div className="mb-1 text-xs">Get in on</div>
+                        <div className="-mt-1 font-sans text-sm font-semibold">Google Play</div>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <style jsx>{`
+        .fade-in-section {
+          opacity: 0;
+          transform: translateY(20px);
+          transition: opacity 2s, transform 2s;
+        }
+        .is-visible {
+          opacity: 1;
+          transform: translateY(0);
+        }
+      `}</style>
+    </>
   )
 }
 
-export default mockup
-
+export default Mockup;
