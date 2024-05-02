9a822033df393a62039cf2ffd34331bd28b200f5
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-05-02 22:06:41 +0530

    Update Testimonials.jsx

--- components/Testimonials.jsx
+++ components/Testimonials.jsx
@@ -4,7 +4,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Link from 'next/link';
-import Image from 'next/Image'
+import Image from 'next/image'
 import { useInView } from 'react-intersection-observer';
 
 const Testimonials = () => {
