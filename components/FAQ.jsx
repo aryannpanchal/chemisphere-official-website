e203e0f73d13d75371cd779e0e0ddb479704e37d
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-05-03 10:19:26 +0530

    Update FAQ.jsx

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -10,25 +10,25 @@ const FAQ = () => {
       </div>
       <div className="p-4 bg-white rounded-lg text-black border border-1 shadow text-md">
         <Accordion
-          title={<h5 className='font-bold text-2xl'>Why choose Chemisphere?</h5>}
+          title={<h5 className='font-bold laptop:text-2xl'>Why choose Chemisphere?</h5>}
           answer = {<p className='pt-2 text-black laptop:text-xl opacity-95'>Choose Chemisphere for a captivating chemistry learning experience! Learn from professionals with a decade of mentoring success. Enjoy engaging live classes, meticulous class notes, and track progress with regular assessments and examinations. Elevate your marks and embrace the joy of learning chemistry!</p>}
           className='lg:text-left gap-14 '
           id='accordian'
         />
         <hr />
         <Accordion
-          title={<h5 className='font-bold  text-2xl'>How will students clarify their doubts?</h5>}
+          title={<h5 className='font-bold  laptop:text-2xl'>Doubt clarification?</h5>}
           answer = {<p className='pt-2 text-black laptop:text-xl opacity-95'>Despite the higher number of hours at other institutes, the challenge lies in providing personalized attention to each student. We are committed to ensuring that students have direct communication with the teacher for doubt clarification through platforms like Telegram and WhatsApp. This allows for a more individualized and focused learning experience.</p>}
           className='lg:text-left'
         /><hr />
         <Accordion
-          title={<h5 className='font-bold text-2xl'>How will tests be conducted?</h5>}
+          title={<h5 className='font-bold laptop:text-2xl'>How will tests be conducted?</h5>}
           answer = {<p className='pt-2 text-black laptop:text-xl opacity-95'>Our coaching program leverages technology to enhance the learning experience. The Android/iOS app facilitates a convenient platform for conducting tests and enables parents to track their child's performance. This integration of technology ensures a modern and efficient learning environment. All the reports of the tests are also shared with the parent so that their performance is tracked.</p>}
           className='lg:text-left svg'
         />        <hr />
 
         <Accordion
-          title={<h5 className='font-bold text-2xl'>Will our child be in safe hands?</h5>}
+          title={<h5 className='font-bold laptop:text-2xl'>Will our child be in safe hands?</h5>}
           answer = {<p className='pt-2 text-black laptop:text-xl opacity-95'>As an IIT Madras graduate with 12 years of teaching experience in various coaching institutes across India, I bring a unique perspective to the table. Unlike some coaching institutes that prioritize completing the syllabus and little focus beyond that, my focus is on understanding individual student needs and providing quality education. My experience allows me to guide students not just through the curriculum sooner but also to help them develop a deeper understanding of the subjects.</p>}
           className='lg:text-left'
         />        <hr />
