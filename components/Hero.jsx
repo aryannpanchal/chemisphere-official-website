cf243e738e061eeadd73190ca179c5c5d86898d2
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-11-20 23:43:27 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
   frameborder="0" 
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
   referrerpolicy="strict-origin-when-cross-origin" 
-  allowfullscreen>
+  allowFullScreen>
 </iframe>
 
 
