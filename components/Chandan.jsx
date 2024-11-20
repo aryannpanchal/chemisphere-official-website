da165080ad76f6a1c08d654cd8119e81fce59633
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-11-20 23:48:17 +0530

    Update Chandan.jsx

--- components/Chandan.jsx
+++ components/Chandan.jsx
@@ -17,10 +17,10 @@ const Chandan = () => {
       {/* Text Section */}
       <div className="laptop:w-1/2 p-5">
         <div className="text">
-          <p className="my-4 font-bold text-3xl mobile:text-center laptop:text-left">
+          <p className="my-4 font-bold laptop:text-3xl mobile:text-2xl mobile:text-center laptop:text-left">
             About <span className="text-chemisphere">Chandan Biswas</span>
           </p>
-          <p className="text-gray-700 font-semibold laptop:text-left mobile:text-justify">
+          <p className="text-gray-700 laptop:text-left mobile:text-justify">
             Chemistry learning is boring.
             But what if I said it also has the potential to dramatically increase your marks and help you get a great rank in your desired exams? Luckily, there is a way!
             Hi. I am Chandan Biswas. I am a professor of Chemistry and CEO of Chemisphere, and I have mentored over 15,000 students in the last decade for JEE Main and Advanced, NEET, MHCET, and Class 12th boards. I know the ins and outs of all these exams. I have had the honor to mentor over 1,000 IITians and medical professionals.
