9370e9403de7270e5f25783774672bc6e0c5c3cf
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2025-07-26 12:13:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,38 @@
-import './globals.css'
+import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import localFont from 'next/font/local'
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Inter } from 'next/font/google';
 import { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs" 
+import { ClerkProvider } from "@clerk/nextjs";
 
-const myFont = localFont({  
-  src: [
-  {
-    path: '../fonts/Montserrat-Regular.ttf',
-  }
-  ] })
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
-  export const metadata: Metadata = {
+export const metadata: Metadata = {
+  title: "Chemisphere",
+  description: "Looking to excel in JEE Main and Advanced, NEET, MHCET, or Class 12th boards? Look no further! Chandan Biswas, CEO of Chemisphere and a renowned Chemistry professor, offers personalized classes and mentorship to help you achieve your goals.",
+  openGraph: {
     title: "Chemisphere",
-    description: "Looking to excel in JEE Main and Advanced, NEET, MHCET, or Class 12th boards? Look no further! Chandan Biswas, CEO of Chemisphere and a renowned Chemistry professor, offers personalized classes and mentorship to help you achieve your goals. ",
-    openGraph: {
-      title: "Chemisphere",
-      description: "Looking to excel in JEE Main and Advanced, NEET, MHCET, or Class 12th boards? Look no further! Chandan Biswas, CEO of Chemisphere and a renowned Chemistry professor, offers personalized classes and mentorship to help you achieve your goals. ",
-      url: "https://www.chemisphere.in/", // Replace with your actual URL
-      siteName: "Chemisphere.in",
-      images: [
-        {
-          url: "https://www.chemisphere.in/meta.png", // Path to your local image
-          width: 1200,  // Adjust width as needed
-          height: 630,  // Adjust height as needed
-          type: "image/png", // Change to 'image/png'
-        },
-      ],
-      type: "website",
-    },
-    robots: {
-      index: true,
-      follow: true,
-    },
-  };
-  
+    description: "Looking to excel in JEE Main and Advanced, NEET, MHCET, or Class 12th boards? Look no further! Chandan Biswas, CEO of Chemisphere and a renowned Chemistry professor, offers personalized classes and mentorship to help you achieve your goals.",
+    url: "https://www.chemisphere.in/",
+    siteName: "Chemisphere.in",
+    images: [
+      {
+        url: "https://www.chemisphere.in/meta.png",
+        width: 1200,
+        height: 630,
+        type: "image/png",
+      },
+    ],
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
 
 export default function RootLayout({
   children,
@@ -43,28 +40,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-<ClerkProvider>
+    <ClerkProvider>
       <html lang="en">
-     <head>
-<meta name="facebook-domain-verification" content="1tc0meng0ifa67yzp1whrppkee1p44" />
-     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
-     <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
-/>
-
-     <link rel="icon" href='/favicon.ico' sizes="any" />
-     </head>
-      <body className={myFont.className}>{children}<Analytics /><SpeedInsights/>
-      </body>
-    </html>
-     </ClerkProvider>   
-    
-  )
+        <head>
+          <meta name="facebook-domain-verification" content="1tc0meng0ifa67yzp1whrppkee1p44" />
+          <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+        </head>
+        <body className={inter.className}>
+          {children}
+          <Analytics />
+          <SpeedInsights />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
-
-
-
-
-
-
