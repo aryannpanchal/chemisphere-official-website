67535dcfc00a19bf56b1ff1af9bfcbe3a8630d1c
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2025-08-07 10:57:53 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from 'next/font/google';
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import Script from 'next/script';
 import { useEffect } from 'react';
