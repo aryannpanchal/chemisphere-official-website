ade03f2c62bb7ad576bb511092bc3ac6eedb9b22
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2025-08-07 10:51:40 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,6 +43,21 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <head>
+        <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1440575971141238');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1440575971141238&ev=PageView&noscript=1"
+/></noscript>
           <meta name="facebook-domain-verification" content="1tc0meng0ifa67yzp1whrppkee1p44" />
           <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css" rel="stylesheet" />
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
